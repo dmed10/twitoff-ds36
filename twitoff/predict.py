a0546431699bb40132ab29eae4f5dbc704939927
Denis Medvedev <mdenis179@gmail.com>
2022-03-31 09:28:04 -0700

    first commit

---
+++ twitoff/predict.py
@@ -0,0 +1,26 @@
+from .models import User
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from .twitter import vectorize_tweet
+
+
+def predict_user(user0_username, user1_username, hypo_tweet_text):
+    user0 = User.query.filter(User.username == user0_username).one()
+    user1 = User.query.filter(User.username == user1_username).one()
+
+    user0_vect = np.array([tweet.vect for tweet in user0.tweets])
+    user1_vect = np.array([tweet.vect for tweet in user1.tweets])
+
+    X = np.vstack([user0_vect, user1_vect])
+
+    y = np.concatenate([np.zeros(len(user0.tweets)),
+                        np.ones(len(user1.tweets))])
+
+    log_reg = LogisticRegression()
+    log_reg.fit(X, y)
+
+    hypo_tweet_vect = np.array([vectorize_tweet(hypo_tweet_text)])
+
+    prediction = log_reg.predict(hypo_tweet_vect)
+
+    return prediction[0]
